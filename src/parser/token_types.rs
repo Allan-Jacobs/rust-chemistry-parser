f3998cbe11abb71a3ea83eda1728eacc7615b60f
Allan Jacobs <ajjacobs01@kellerisd.com>
2022-03-25 10:52:33 -0500

    add token types

---
+++ src/parser/token_types.rs
@@ -0,0 +1,34 @@
+trait Token {}
+
+#[derive(Debug)]
+enum ParenType {
+    OPEN,
+    CLOSE
+}
+
+#[derive(Debug)]
+pub struct Element {
+    value: &'static str // TODO: use enum
+}
+
+#[derive(Debug)]
+pub struct Number {
+    value: i16
+}
+
+#[derive(Debug)]
+pub struct Paren {
+    value: ParenType
+}
+
+#[derive(Debug)]
+pub struct Plus();
+
+#[derive(Debug)]
+pub struct Yields();
+
+impl Token for Element {}
+impl Token for Number {}
+impl Token for Paren {}
+impl Token for Plus {}
+impl Token for Yields {}
