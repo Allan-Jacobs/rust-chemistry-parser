7d4dab91453680c06d9a1716bf48824231d19156
Allan Jacobs <ajjacobs01@kellerisd.com>
2022-03-26 15:27:18 -0500

    make fields public change the type of element to be a string rather than a &'static str

--- src/parser/token_types.rs
+++ src/parser/token_types.rs
@@ -1,24 +1,24 @@
 pub trait Token {}
 
 #[derive(Debug)]
-enum ParenType {
+pub enum ParenType {
     OPEN,
     CLOSE
 }
 
 #[derive(Debug)]
 pub struct Element {
-    value: &'static str // TODO: use enum
+    pub value: String // TODO: use enum
 }
 
 #[derive(Debug)]
 pub struct Number {
-    value: i16
+    pub value: i16
 }
 
 #[derive(Debug)]
 pub struct Paren {
-    value: ParenType
+    pub value: ParenType
 }
 
 #[derive(Debug)]
