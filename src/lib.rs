9582c02afef06ae7bb4e43abc0215cfac349e242
Allan Jacobs <ajjacobs01@kellerisd.com>
2022-03-30 12:29:40 -0500

    move mod.rs functionality to lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -4,3 +4,11 @@ pub mod parser;
 pub mod lexer;
 pub mod ast_types;
 pub mod token_types;
+
+use self::{ast_types::Node, lexer::LazyTokenStream};
+
+/// Parse a string and return a result with either the node or the error 
+pub fn parse(string: String) -> Result<Box<Node>, String> {
+    let stream = LazyTokenStream::new(&string);
+    parser::parse(stream)
+}
