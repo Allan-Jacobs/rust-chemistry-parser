47c2171349b1deb95d0fd5f004273fdb60fec4f1
Allan Jacobs <ajjacobs01@kellerisd.com>
2022-05-12 14:30:57 -0500

    Modify Tokens enum to allow for added metadata

--- src/token_types.rs
+++ src/token_types.rs
@@ -9,16 +9,46 @@ pub enum ParenType {
 }
 
 /// A token which may have attached data
-#[derive(Debug, PartialEq)]
+#[derive(Debug)]
 pub enum Tokens {
     /// Elements e.g. Fe. The data is "Fe"
-    Element(String),
+    Element { data: String, meta: TokenMetadata },
     /// Numbers e.g. 13. The data is 13u16
-    Number(u16),
+    Number { data: u16, meta: TokenMetadata },
     /// Parenthesis e.g. ) The data is ParenType::CLOSE
-    Paren(ParenType),
+    Paren { data: ParenType, meta: TokenMetadata },
     /// Plus sign +
-    Plus,
+    Plus { meta: TokenMetadata},
     /// Yields sign -> 
-    Yields,
+    Yields { meta: TokenMetadata },
+}
+
+impl Tokens {
+    pub fn meta(&self) -> TokenMetadata {
+        match *self {
+            Self::Element { meta, data: _ } => meta,
+            Self::Number { meta, data: _ } => meta,
+            Self::Paren { meta, data: _ } => meta,
+            Self::Plus { meta} => meta,
+            Self::Yields { meta} => meta
+        }
+    }
+}
+
+impl PartialEq for Tokens {
+    fn eq(&self, other: &Self) -> bool {
+        match (self, other) {
+            (Self::Element { data: l_data, meta: _ }, Self::Element { data: r_data, meta: r_meta }) => l_data == r_data,
+            (Self::Number { data: l_data, meta: _ }, Self::Number { data: r_data, meta: r_meta }) => l_data == r_data,
+            (Self::Paren { data: l_data, meta: _ }, Self::Paren { data: r_data, meta: r_meta }) => l_data == r_data,
+            (Self::Plus { meta: l_meta }, Self::Plus { meta: _ }) => true,
+            (Self::Yields { meta: l_meta }, Self::Yields { meta: _ }) => true,
+        }
+    }
+}
+
+#[derive(Debug)]
+pub struct TokenMetadata {
+    raw: String,
+    location: usize
 }
